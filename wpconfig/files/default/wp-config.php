7c5363ecbf1e7848c31ba2dae4a78c63ba94a981
Sean Yang <comdaze@163.com>
2018-12-21 00:42:13 +0800

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'wordpress.rqtwxz.0001.apse1.cache.amazonaws.com');
  
-define('WP_HOME','');
-define('WP_SITEURL','');
+define('WP_HOME','d1ftii7wita2jt.cloudfront.net');
+define('WP_SITEURL','d1ftii7wita2jt.cloudfront.net');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
